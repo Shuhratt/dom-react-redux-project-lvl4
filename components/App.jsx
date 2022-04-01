ee4a73629a736c007cf6454f835f595b675ba1e9
shuh <shuh47@yandex.ru>
2022-04-01 13:16:21 +0300

    add login style

--- components/App.jsx
+++ components/App.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
+import Main from "./Main";
+import Login from "./Login";
 
 export function App() {
-  return <div>"React начало"</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="*" element={<Navigate to="/login" />} />
+        <Route exact path="/" element={<Main />} />
+        <Route exact path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- components/Login.jsx
+++ components/Login.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import styles from "../assets/Login.module.scss";
 
 const Login = () => {
-  return <div>1</div>;
+  return (
+    <div className="col-12 col-sm-8 mx-auto">
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form className={styles.login}>
+            <div className={styles.login__box}>
+              <h1 className="text-center">Войти</h1>
+              <Field type="email" name="email" />
+              <ErrorMessage name="email" component="div" />
+              <Field type="password" name="password" />
+              <ErrorMessage name="password" component="div" />
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className="btn btn-secondary"
+              >
+                Войти
+              </button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 };
 
 export default Login;
