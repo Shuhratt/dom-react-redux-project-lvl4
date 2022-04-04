2c074066dfa341042d3d29405151404c9018df78
shuh <shuh47@yandex.ru>
2022-04-04 18:33:19 +0300

    fix

--- components/Main.jsx
+++ components/Main.jsx
@@ -15,13 +15,13 @@ const Main = () => {
           </div>
         </div>
       </nav>
-      <div className="container h-100 my-4 overflow-hidden rounded shadow">
-        <div className="row h-100 bg-white flex-md-row">
-          {/*<div className="col-12">Header {isAuthed && "Вошёл"}</div>*/}
+      <div className="container my-4 overflow-hidden rounded shadow flex-grow-1">
+        <div className="row bg-white flex-md-row h-100">
           <div className="col-4 col-md-2 border-end pt-5 px-0 bg-light">
             Cлева
+            {isAuthed && "Вошёл"}
           </div>
-          <div className="col p-0 h-100">Контент</div>
+          <div className="col p-0">Контент</div>
         </div>
       </div>
     </div>
